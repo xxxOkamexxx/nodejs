583640172e881344ca6781da403da129c92d13d1
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-02-02 17:23:13 +0100

    path to directory  and to the current file.

--- 01-basics/01-globals.js
+++ 01-basics/01-globals.js
@@ -3,8 +3,11 @@
  */
 
 console.log("Hello world!");
-
+/*
 setTimeout(() => {
     console.log("Ill be back!");
 },3000);
+*/
 
+console.log("Absolute path to directory for this file: ",__dirname);
+console.log("Absolute path to this file: ",__filename);
