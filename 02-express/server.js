a7bb3f913a3639654891b63d570b5d22b7b9d4cf
xxxOkamexxx <94180822+xxxOkamexxx@users.noreply.github.com>
2022-02-02 21:13:51 +0100

    demonstration about request and respond

--- 02-express/server.js
+++ 02-express/server.js
@@ -1,3 +1,25 @@
+/**
+ * express
+ */
 
 const express = require('express');
+const { sep } = require('path/posix');
+const app = express();
 
+
+// Respond to GET request for `/`
+app.get('/',(req, res) => {
+    // req = information om den inkommande förfrågan
+	// res = metoder för att skicka ett svar på förfrågan
+    console.log(req.method, req.url);
+
+    res.send('Hello fron root😵😵‍💫🤯!');
+});
+
+
+
+
+// Start listening for incoming requests on port 3000
+app.listen(3000, () => {
+    console.log(`server started at http://localhost:3000`);
+});
